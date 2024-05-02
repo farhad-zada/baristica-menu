8bbfbe469f5bbebf18229a70e443342d349315a4
nihadaev <nihadaev@mail.ru>
2024-05-02 12:27:00 +0400

    swiper update

--- assets/js/coffePage.js
+++ assets/js/coffePage.js
@@ -2,6 +2,10 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: '1',
     spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 
 const coffeContent = [
